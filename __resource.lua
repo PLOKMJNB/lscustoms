4ce4ecf3b088b6961d187a459b8f383f84f2cbda
Danish Khan <61810778+PLOKMJNB@users.noreply.github.com>
2020-11-04 14:37:00 -0500

    upload

---
+++ __resource.lua
@@ -0,0 +1,5 @@
+client_script 'menu.lua'
+client_script 'lscustoms.lua'
+server_script 'lscustoms_server.lua'
+client_script 'lsconfig.lua'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
