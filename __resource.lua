5702ba232d70031ed1a236fede9d2304b741d996
Hamy-os <hamzaalisic0@gmail.com>
2021-01-23 18:59:00 +0300

    Updated resource.lua to fxmanifest.lua
    Updated resource.lua to fxmanifest.lua

--- __resource.lua
+++
@@ -1,5 +0,0 @@
-client_script 'menu.lua'
-client_script 'lscustoms.lua'
-server_script 'lscustoms_server.lua'
-client_script 'lsconfig.lua'
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Ploko'
+description 'lscustoms'
+version '1.0.0'
+
+client_scripts {
+    'lsconfig.lua',
+    'menu.lua',
+    'lscustoms.lua'
+}
+server_script 'lscustoms_server.lua'
